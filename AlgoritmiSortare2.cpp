1e1506667acee4002b6dd65d3693ebf30a755707
Sergiu Popistas <sergiu.popistas@itschool.ro>
2024-06-06 19:55:44 +0300

    merge sort

--- AlgoritmiSortare2.cpp
+++ AlgoritmiSortare2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 //printeze un array de tip int
 void printArray(int my_arr[], int dimens)
 {
@@ -16,7 +17,7 @@ void swap(int* a, int* b)
     *a = *b;
     *b = temp;
 }
-
+int* t = new int;
 //O(n2)
 //Functia care implementeaza bubbkeSort
 void bubbleSort(int arr[], int dimens)
@@ -35,6 +36,85 @@ void bubbleSort(int arr[], int dimens)
         }
     }
 }
+
+
+//Functie pentru a fuziona doua sub-arrayuri in ordine corecta
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;//Dimensiunea primului sub array
+    int n2 = right - mid; // Dimensunea celui de-al doilea array
+
+    // creem doua array-uri temporare
+    std::vector<int> L(n1);
+    std::vector<int> R(n2);
+
+    for (int i = 0; i < n1; i++)
+    {
+        L[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++)
+    {
+        R[j] = arr[mid + 1 + j];
+    }
+
+    //Index initial pt ptrimul si al doilea su-array
+    int i = 0, j = 0;
+    
+    //Indexul initial pt array fuzionat
+    int k = left;
+
+    //Fuzionam sub-arrayl inapopi in array-ul original
+    while (i<n1  && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+    }
+    //Copiem elementele ramase din L[] daca exista
+    while (i<n1)
+    {
+        arr[k] = L[k];
+        i++;
+        k++;
+    }
+    //Copiem elementele ramase din R[] daca exista
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+
+
+
+
+//Functia principala pentru sortarea arra=ului folosind Merge Sort
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        //Gasim mijlocul array-ului
+        int mid = left + (right - left) / 2;
+
+        //Sortam prima si a doua jumatatte
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+
+        //Fuzionam jumatatile sortate
+        //merge
+    }
+}
+
 
 int main()
 {
